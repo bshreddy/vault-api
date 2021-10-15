340ef4a4572257ba0d2368169645d6380e2c3803
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-10-15 17:21:03 +0530

    Removed old interfaces

--- lib/core/vault.ts
+++ lib/core/vault.ts
@@ -2,65 +2,27 @@ import {request} from '../engines';
 import {Config, DefaultConfig, RequestConfig} from '../types';
 
 /* eslint-disable no-unused-vars */
-type FunctionWithoutData = (path: string) => Promise<any>;
-type FunctionWithData = (path: string, data: any) => Promise<any>;
+// type FunctionWithoutData = (path: string) => Promise<any>;
+// type FunctionWithData = (path: string, data: any) => Promise<any>;
 
-interface IVault {
-    [key: string]: any;
-
-    read: FunctionWithoutData,
-    list: FunctionWithoutData,
-    delete: FunctionWithoutData,
-    help: FunctionWithoutData,
-
-    write: FunctionWithData,
-}
-
-export interface VaultFunc extends IVault {
+export interface VaultFunc{
     (config: Config): Promise<any>,
     create?: (defaultConfig: DefaultConfig) => VaultFunc;
 }
 /* eslint-enable no-unused-vars */
 
-export class Vault implements IVault {
-    // eslint-disable-next-line no-undef
-    [key: string]: any;
-
+export class Vault {
     private defaults: DefaultConfig;
 
     private methodsWithoutData = ['read', 'list', 'delete', 'help'];
     private methodsWithData = ['write'];
 
-    public read!: FunctionWithoutData;
-    public list!: FunctionWithoutData;
-    public delete!: FunctionWithoutData;
-    public help!: FunctionWithoutData;
-    public write!: FunctionWithData;
     public vault!: VaultFunc;
 
     constructor(instanceConfig: DefaultConfig) {
         this.defaults = instanceConfig;
 
-        this.methodsWithoutData.forEach((method) => {
-            this[method] = async (path: string): Promise<any> => {
-                return this.vault({method, path});
-            };
-        });
-
-        this.methodsWithData.forEach((method) => {
-            this[method] = async (path: string, data: any): Promise<any> => {
-                return this.vault({method, path, data});
-            };
-        });
-
-        // #TODO: Update this with loops
-        this.vault = Object.assign(this._vault.bind(this), {
-            read: this.read,
-            list: this.list,
-            'delete': this.delete,
-            help: this.help,
-            write: this.write,
-        });
+        this.vault = Object.assign(this._vault.bind(this), {});
     }
 
     private async _vault(config: Config): Promise<any> {
