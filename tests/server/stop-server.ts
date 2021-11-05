071af3eeb2fe99e4e11c5c44e825965e087bb5c2
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-11-05 13:28:05 +0530

    Added Github CI Tests

--- tests/server/stop-server.ts
+++ tests/server/stop-server.ts
@@ -4,12 +4,15 @@ export function stopServer(vaultServer: VaultServer | null): Promise<void> {
     return new Promise((resolve, reject) => {
         if (!vaultServer || vaultServer.server.killed) { resolve(); }
 
+        console.log(vaultServer?.server);
+
         vaultServer?.server.on('close', () => {
             vaultServer = null;
             delete process.env.VAULT_ADDR;
             delete process.env.TOKEN;
             delete process.env.UNSEAL_KEY;
 
+            console.log('Vault server stopped');
             resolve();
         });
 
--- tests/setup/global-teardown.ts
+++ tests/setup/global-teardown.ts
@@ -1,6 +1,8 @@
 import {stopServer} from '../server';
 
 export async function globalTeardown(): Promise<void> {
+    console.log('globalTeardown');
+
     await stopServer();
 }
 
