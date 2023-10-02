a070d3bbcbcfe0287f2bcded251b5d6e6cec308d
Antoine <15737031+MinDBreaK@users.noreply.github.com>
2023-10-02 19:09:38 +0200

    Updated types of Vault Response (#13)

--- lib/types/vault-response.ts
+++ lib/types/vault-response.ts
@@ -1,13 +1,22 @@
 import {Dictionary} from '.';
 
+export type WrapInfo = {
+    token: string,
+    accessor: string,
+    ttl: number,
+    creation_time: string,
+    creation_path: string,
+    wrapped_accessor: string,
+}
+
 export interface VaultResponse extends Dictionary<any> {
     request_id: string;
     lease_id: string;
     renewable: boolean;
     lease_duration: number;
-    data: Dictionary<any>;
+    data: Dictionary<any> | null;
     metadata?: Dictionary<any>;
-    wrap_info?: Dictionary<any> | null;
+    wrap_info?: WrapInfo | null;
     warnings?: any | null;
     auth?: any | null;
     statusCode: number;
